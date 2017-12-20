e1af5dd4b5f2658f602b6620c02f2531003b6e84
Sam Singer <samuel.singer1@gmail.com>
2017-12-20 00:06:12 -0700

    Header and routes set up

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import rootReducer from './reducers/index';
-// import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Store from './Store';
-
-// const devTools =
-//   window.__REDUX_DEVTOOLS_EXTENSION__
-//   && window.__REDUX_DEVTOOLS_EXTENSION__()
-// const store = createStore(rootReducer, devTools)
 
 ReactDOM.render(
   <Provider store = { Store }>
@@ -20,4 +13,4 @@ ReactDOM.render(
     </BrowserRouter>
   </Provider>,
     document.getElementById('root')
-  );
+);
