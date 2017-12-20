4f4aaaecfcafc622dda82493865f2e0d65656ef7
Sam Singer <samuel.singer1@gmail.com>
2017-12-19 23:23:01 -0700

    Set-up BrowserRouter and Thunk Middleware

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import AddMessage from '../../containers/AddMessage/AddMessage';
+import Header from '../Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <AddMessage />
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <AddMessage />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import rootReducer from './reducers/index';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Store from './Store';
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__
-  && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(rootReducer, devTools)
+// const devTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__
+//   && window.__REDUX_DEVTOOLS_EXTENSION__()
+// const store = createStore(rootReducer, devTools)
 
 ReactDOM.render(
-  <Provider store = { store }>
-    <App />
+  <Provider store = { Store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
     document.getElementById('root')
   );
