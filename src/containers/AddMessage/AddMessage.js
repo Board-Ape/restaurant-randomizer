111c36b0484b609094a1eb7bbc2a4f8d254e7fe1
Sam Singer <samuel.singer1@gmail.com>
2017-12-21 15:18:53 -0700

    Message through redux is now working

--- src/containers/AddMessage/AddMessage.js
+++ src/containers/AddMessage/AddMessage.js
@@ -2,35 +2,42 @@ import React, { Component } from 'react';
 import './AddMessage.css';
 import { connect } from 'react-redux';
 import { addEventName } from '../../actions';
+import MessageCard from '../../components/MessageCard/MessageCard';
 
 class AddMessage extends Component {
   constructor() {
     super()
     this.state = {
-      title: ''
+      eventTitle: ''
     }
   }
 
   handleChange = (event) => {
-    this.setState({title: event.target.value})
+    this.setState({eventTitle: event.target.value})
   }
 
   submitEventName = (event) => {
     event.preventDefault();
-    this.props.handleSubmit(this.state.title, this.props.eventName.length)
+    this.props.handleSubmit(this.state.eventTitle, this.props.eventName.length)
   }
 
   render() {
+    const messagesArray = this.props.eventName
+    const messageCardArray = messagesArray.map(messages => {
+      return (<MessageCard key={this.props.eventName}
+                            eventName={this.props.eventName} />)
+    })
     return(
       <div className='add-message-container'>
         <h1>AddMessage!</h1>
         <form onSubmit={this.submitEventName}>
           <input type='text'
                   placeholder='Event Name'
-                  value={this.state.title}
+                  value={this.state.eventTitle}
                   onChange={this.handleChange} />
           <input type='submit' />
         </form>
+        {messageCardArray}
       </div>
     )
   }
@@ -42,8 +49,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleSubmit: (title, id) => {
-      dispatch(addEventName(title, id))
+    handleSubmit: (eventTitle, id) => {
+      dispatch(addEventName(eventTitle, id))
     }
   }
 }
