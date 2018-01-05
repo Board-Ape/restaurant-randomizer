865998e5a6cf1a17775f2b33737617f657d7a3e3
Sam Singer <samuel.singer1@gmail.com>
2018-01-05 12:27:22 -0700

    Reducers Tests Initial

--- src/reducers/reducers-test/reducers.test.js
+++ src/reducers/reducers-test/reducers.test.js
@@ -1,1 +1,26 @@
+import eventNameReducer from '../eventNameReducer';
+import * as actions from '../../actions';
 
+describe('Reducers', () => {
+
+  it('should have an initial state', () => {
+    const expected = [];
+
+    expect(eventNameReducer(undefined, [])).toEqual(expected)
+  });
+
+  it.skip('should return a new state when an action is called', () => {
+    const mockObject = {name: 'Sam', id: 0}
+    const expected = {name: 'Sam', id: 0}
+
+    expect(eventNameReducer([], actions.addEventName(mockObject))).toEqual(expected)
+  });
+
+  // it('should return a new state with an action', () => {
+  //   const houseMembers = {aloy: 'Metal', prim: 'Rose'};
+  //   const expected = houseMembers;
+  //
+  //   expect(members({}, actions.fetchMembersSuccess(houseMembers))).toEqual(expected);
+  // });
+
+});
