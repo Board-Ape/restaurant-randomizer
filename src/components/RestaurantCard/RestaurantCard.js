9f40149d326f31d1793d5b752fbfe2bb247419f9
Sam Singer <samuel.singer1@gmail.com>
2018-01-07 20:46:19 -0700

    Card Info Is Complete

--- src/components/RestaurantCard/RestaurantCard.js
+++ src/components/RestaurantCard/RestaurantCard.js
@@ -3,12 +3,14 @@ import './RestaurantCard.css';
 import PropTypes from 'prop-types';
 
 const RestaurantCard = (props) => {
-  console.log(props);
   return (
     <div className='card-container'>
       <div className='cards'>
         <h1 className='restaurant-name'>{props.restaurants.name}</h1>
-        <h2 className='address'>Address: {props.restaurants.data.Address}</h2>
+        <h2 className='rating'>Rating: {props.restaurants.data.Rating}</h2>
+        <h2 className='cuisine'>Cuisine: {props.restaurants.data.Cuisines}</h2>
+        <h2 className='address'>Address:</h2>
+        <h2 className='address'>{props.restaurants.data.Address}</h2>
       </div>
     </div>
   );
@@ -17,5 +19,10 @@ const RestaurantCard = (props) => {
 export default RestaurantCard;
 
 RestaurantCard.propTypes = {
-  title: PropTypes.string
+  restaurants: PropTypes.object,
+  name: PropTypes.string,
+  data: PropTypes.object,
+  Rating: PropTypes.string,
+  Cuisines: PropTypes.string,
+  Address: PropTypes.string
 };
