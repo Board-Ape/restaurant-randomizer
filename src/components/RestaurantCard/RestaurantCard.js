efdfd4330478382468ddcb4310f692b8c1b9d0c8
Sam Singer <samuel.singer1@gmail.com>
2018-01-07 23:37:36 -0700

    Card Colors And Font-Family

--- src/components/RestaurantCard/RestaurantCard.js
+++ src/components/RestaurantCard/RestaurantCard.js
@@ -6,8 +6,8 @@ const RestaurantCard = (props) => {
   return (
     <div className='card-container'>
       <div className='cards'>
-        <h1 className='restaurant-name'>{props.restaurants.name}</h1>
         <h2 className='rating'>Rating: {props.restaurants.data.Rating}</h2>
+        <h1 className='restaurant-name'>{props.restaurants.name}</h1>
         <h2 className='cuisine'>Cuisine: {props.restaurants.data.Cuisines}</h2>
         <h2 className='address'>Address:</h2>
         <h2 className='address'>{props.restaurants.data.Address}</h2>
