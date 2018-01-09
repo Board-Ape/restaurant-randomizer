5729a8b9af29ed2ca28e41efdd5d40c62c7ba853
Sam Singer <samuel.singer1@gmail.com>
2018-01-09 13:17:59 -0700

    Basic Favorite Route Initial Set Up

---
+++ src/containers/FavoritesContainer/FavoritesContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import RestaurantCard from '../../components/RestaurantCard/RestaurantCard';
+// import './FavoritesContainer.css';
+import PropTypes from 'prop-types';
+
+class FavoritesContainer extends Component {
+  render() {
+    return (
+      <div>
+      Hello
+      </div>
+    );
+  }
+}
+
+export default FavoritesContainer;
